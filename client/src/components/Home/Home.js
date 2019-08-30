3852fe60211d0deddc333563595c8206dafca6a3
Keenan <keol2728@colorado.edu>
2019-08-30 16:21:06 -0700

    adding copy to home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,13 +9,11 @@ class Home extends Component {
 			<div className={classes.Home}>
 				<div className={classes.LeftCol}>
 					<h1>
-						Lorem ipsum <span>dolor</span>
+						We <span>Scale</span>
 					</h1>
-					<h2>Excepteur sint occaecat cupidatat</h2>
+					<h2>Impactful Ideas & Ventures Accross the Globe</h2>
 					<h3>
-						Lacus vel facilisis volutpat est velit egestas dui. Pellentesque
-						elit ullamcorper dignissim cras tincidunt. Tincidunt lobortis
-						feugiat vivamus at augue eget.
+						WE ARE A GLOBAL NETWORK FOR BUSINESS LEADERS, DATA SCIENTISTS & SOFTWARE ENGINEERS.
 					</h3>
 					<EmailForm />
 				</div>
