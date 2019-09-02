65042e4769ede2421cead1f06c8b4a315bbc5ca2
Keenan <keol2728@colorado.edu>
2019-09-02 14:25:10 -0600

    changing about image

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -7,7 +7,7 @@ class About extends Component {
 	render() {
 		return (
 			<div className={classes.About}>
-				<div className={classes.LeftCol}>
+				<div className={classes.RightCol}>
 					<h1>
 						About Us: <span>We Scale Impactful Ideas & Ventures Worldwide</span>
 					</h1>
@@ -15,7 +15,7 @@ class About extends Component {
 						WE ARE A GLOBAL NETWORK FOR BUSINESS LEADERS, DATA SCIENTISTS & SOFTWARE ENGINEERS.
 					</h3>
 				</div>
-				<div className={classes.RightCol}/>
+				<div className={classes.LeftCol}/>
 			</div>
 		);
 	}
